5019dcfb633e08b25bf9b500d145fc488e88cf94
dns <shulgaol@mail.ru>
2022-11-14 16:12:26 +0200

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,68 @@
 public class Main {
     public static void main(String[] args) {
         byte cup = 1;
-        System.out.println("Значение переменной "+"cup"+" с типом "+"byte"+" равно "+cup);
+            System.out.println("Значение переменной "+"cup"+" с типом "+"byte"+" равно "+cup);
         short pen = 2;
-        System.out.println("Значение переменной "+"pen"+" с типом "+"short"+" равно "+pen);
+            System.out.println("Значение переменной "+"pen"+" с типом "+"short"+" равно "+pen);
         int book = 10;
-        System.out.println("Значение переменной "+"book"+" с типом "+"int"+" равно "+book);
+            System.out.println("Значение переменной "+"book"+" с типом "+"int"+" равно "+book);
         long library = 200L;
-        System.out.println("Значение переменной "+"library"+" с типом "+"long"+" равно "+library);
+            System.out.println("Значение переменной "+"library"+" с типом "+"long"+" равно "+library);
         float page = book+1;
-        System.out.println("Значение переменной "+"page"+" с типом "+"float"+" равно "+page);
+            System.out.println("Значение переменной "+"page"+" с типом "+"float"+" равно "+page);
         double paper = 1.12345678;
-        System.out.println("Значение переменной "+"paper"+" с типом "+"double"+" равно "+paper);
+            System.out.println("Значение переменной "+"paper"+" с типом "+"double"+" равно "+paper);
+
+        double sea = 27.12;
+            System.out.println(sea);
+
+        long sky = 987678965549L;
+        System.out.println(sky);
+
+        byte fox = 2;
+        System.out.println(fox);
+
+        short sun = 786;
+        System.out.println(sun);
+
+        boolean ad = false;
+        System.out.println(ad);
+
+        short we = 569;
+        System.out.println(we);
+
+        short oil = -159;
+        System.out.println(oil);
+
+        short cat = 27897;
+        System.out.println(cat);
+
+        byte dog = 67;
+        System.out.println(dog);
+
+        byte son;
+        son = 1;
+            System.out.println(son);
+        short car;
+        car=5;
+            System.out.println(car);
+        int bed;
+        bed = 2;
+            System.out.println(bed);
+        long man;
+        man = 100L;
+            System.out.println(man);
+        float bag;
+        bag = bed+1;
+            System.out.println(bag);
+        double day;
+        day = 100.12345678;
+            System.out.println(day);
+        boolean he;
+        he = true;
+            System.out.println(he);
+        char a;
+        a=50;
+            System.out.println("Задача номер "+a+" этой домашки выполнена");
     }
 }
