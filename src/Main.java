435465b97caa66c1425994f13d4c650bef0752e9
dns <shulgaol@mail.ru>
2022-11-14 15:29:58 +0200

    задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        byte cup = 1;
+        System.out.println("Значение переменной "+"cup"+" с типом "+"byte"+" равно "+cup);
+        short pen = 2;
+        System.out.println("Значение переменной "+"pen"+" с типом "+"short"+" равно "+pen);
+        int book = 10;
+        System.out.println("Значение переменной "+"book"+" с типом "+"int"+" равно "+book);
+        long library = 200L;
+        System.out.println("Значение переменной "+"library"+" с типом "+"long"+" равно "+library);
+        float page = book+1;
+        System.out.println("Значение переменной "+"page"+" с типом "+"float"+" равно "+page);
+        double paper = 1.12345678;
+        System.out.println("Значение переменной "+"paper"+" с типом "+"double"+" равно "+paper);
+    }
+}
